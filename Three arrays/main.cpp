d4e43eb3b4380c8d992fd92688fe841482d9487f
Khalid <khalid@Khalids-MacBook-Pro.local>
2017-09-01 09:10:38 -0400

    Initial Commit

---
+++ Three arrays/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Three arrays
+//
+//  Created by Khalid  on 9/1/17.
+//  Copyright © 2017 Khalid Alghamdi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
